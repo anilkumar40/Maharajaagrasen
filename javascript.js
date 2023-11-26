3fb8e9f9fdc9ea0e8df168497b32963346233cc5
Amar Singh <somyaenterprises526@gmail.com>
2023-11-26 12:50:03 +0530

    my name

--- javascript.js
+++ javascript.js
@@ -4,6 +4,11 @@ const person = {
     age: 50,
     eyeColor: "blue"
   };
-
-
+  const person1 = {
+    firstName: "anil",
+    lastName: "kumar",
+    age: 35,
+    eyeColor: "black"
+  };
 console.log("Hello world!"+person.firstName+" "+person.lastName+person.age+person.eyeColor);
+console.log("Hello world!"+person1.firstName+" "+person1.lastName+person1.age+person1.eyeColor);
