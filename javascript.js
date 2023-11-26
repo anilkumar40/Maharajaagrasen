7da49379b55e4365eb7468d64b28fbe042097490
Amar Singh <somyaenterprises526@gmail.com>
2023-11-26 12:18:58 +0530

    add object

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+
+
+console.log("Hello world!"+person.firstName+" "+person.lastName+person.age+person.eyeColor);
